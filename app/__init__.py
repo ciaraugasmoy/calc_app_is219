d48bc565deef1884f19bf1721feebda32965b745
ciaraugasmoy <ccu3@njit.edu>
2024-03-10 01:00:38 -0500

    logs

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 import pkgutil
 import importlib
+import logging
 from app.commands import CommandHandler
 from app.commands import Command
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 class App:
-    def __init__(self): # Constructor
+    def __init__(self):  # Constructor
         self.command_handler = CommandHandler()
 
     def load_plugins(self):
@@ -18,11 +23,19 @@ class App:
                     try:
                         if issubclass(item, (Command)):  # Assuming a BaseCommand class exists
                             self.command_handler.register_command(plugin_name, item())
+                            logger.info(f'Registered command {item_name} from plugin {plugin_name}')
                     except TypeError:
                         continue  # If item is not a class or unrelated class, just ignore
+
     def start(self):
         # Register commands here
         self.load_plugins()
-        print("Type 'exit' to exit.")
-        while True:  #REPL Read, Evaluate, Print, Loop
-            self.command_handler.execute_command(input(">>> ").strip())
+        logger.info("Type 'exit' to exit.")
+        while True:  # REPL Read, Evaluate, Print, Loop
+            user_input = input(">>> ").strip()
+            logger.info(f"User input: {user_input}")
+            self.command_handler.execute_command(user_input)
+
+if __name__ == "__main__":
+    app = App()
+    app.start()
